4978f63081c918f2308e8f165f58b9d0422ec38b
Frankie McGough <fmcgough@live.co.uk>
2018-11-11 19:09:39 +0000

    Add CORS configuration, currently extremely permissive

--- src/main/kotlin/com/rescuehub/rescuehubserver/RescuehubServerApplication.kt
+++ src/main/kotlin/com/rescuehub/rescuehubserver/RescuehubServerApplication.kt
@@ -3,10 +3,27 @@ package com.rescuehub.rescuehubserver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.context.annotation.Bean
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
-class RescuehubServerApplication
+class RescuehubServerApplication {
+
+    /**
+     * Configuration for cross-origin HTTP requests, so that the UI can access the API.
+     */
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object: WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry): Unit {
+                // TODO get allowed origins from config
+                registry.addMapping("/**").allowedOrigins("*")
+            }
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<RescuehubServerApplication>(*args)
